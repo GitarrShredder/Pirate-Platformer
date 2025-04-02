e920285374d7ed5f5b7805b81041be91b919ad8e
JohanWeigel <johanweigel@mac.com>
2025-04-02 21:51:32 +0200

    Sfx. Organized first scene.

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -14,12 +14,21 @@ class_name Character extends CharacterBody2D
 @export var _air_control: float = 0.5
 @export var _jump_dust: PackedScene
 var _jump_velocity: float 
+var _was_on_floor : bool
 
 @export_category("Sprite")
 @export var _is_facing_left : bool
 @export var _sprites_face_left : bool
 @onready var _sprite_2d: Sprite2D = $Sprite2D
-var _was_on_floor : bool
+
+
+@export_category("swim")
+@export var _density : float = -0.1
+@export var _drag : float = 0.5
+var _water_surface_height : float
+var _is_in_water : bool
+var _is_below_surface : bool
+
 
 signal changed_direction(_is_facing_left : bool)  
 signal landed(floor_height : float)
@@ -45,23 +54,37 @@ func run(direction : float):
 	_direction = direction
 	
 func jump():
-	if is_on_floor():
+	if _is_in_water:
+		if _is_below_surface:
+			velocity.y = _jump_velocity * _drag
+			landed.emit(position.y)
+		else:
+			velocity.y = _jump_velocity
+	
+	elif is_on_floor():
 		velocity.y = _jump_velocity
 		_spawn_dust(_jump_dust)
 		
 func stop_jump():
-	if velocity.y < 0: #decreasing?
+	if velocity.y < 0 and not _is_in_water: #decreasing?
 		velocity.y = 0
 #endregion
 
-func enter_water(water_surface_hight : float):
-	pass
+func enter_water(water_surface_height : float):
+	_water_surface_height = water_surface_height
+	_is_in_water = true
+	_is_below_surface = false
+	landed.emit(position.y)
+	if velocity.y > 0:
+		velocity.y *= _drag
 	
 func exit_water():
-	pass
+	_is_in_water = false
+	
 
 func dive():
-	pass
+	_is_below_surface = true
+	
 
 func _ready() -> void:
 	_speed *= Global.ppt
@@ -76,8 +99,10 @@ func _physics_process(delta: float) -> void:
 		face_left()
 	elif _is_facing_left && sign(_direction) ==1:
 		face_right()
+	if _is_in_water:
+		_water_physics(delta)
 	
-	if is_on_floor():
+	elif is_on_floor():
 		_ground_physics(delta)
 	else:
 		_air_physics(delta)
@@ -101,6 +126,18 @@ func _air_physics(delta :float):
 	if _direction:
 		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
 
+func _water_physics(delta: float):
+	if _direction == 0:
+		velocity.x = move_toward(velocity.x, 0, _deceleration * _drag * delta)
+	else:
+		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _drag * delta)
+	if _is_below_surface or _density >0:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag, gravity * _drag * delta)
+	elif position.y - float(Global.ppt) / 4 >_water_surface_height:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag, gravity * _drag * delta)
+	else:
+		velocity.y = move_toward(velocity.y, gravity * _density * _drag * -1, gravity * _drag * delta)
+		
 func _landed():
 	landed.emit(position.y)
 
--- Scripts/water.gd
+++ Scripts/water.gd
@@ -1,8 +1,45 @@
 extends Area2D
 
+@export var _splash : PackedScene
+@onready var _splashsfx: AudioStreamPlayer2D = $WaterSplash
+@onready var _splash_exit: AudioStreamPlayer2D = $WaterSplash2
 
+@onready var timer: Timer = $Timer
+@onready var timer_2: Timer = $Timer2
+
+var _splashsfx_has_played: bool = false
+var _splash_exit_has_played : bool = false
+
+
+func _spawn_splash_enter(x: float):
+	var splash = _splash.instantiate()
+	add_child(splash)
+	splash.global_position.x = x
+	if _splashsfx_has_played == true:
+		return
+	else:
+		_splashsfx.play()
+		_splashsfx_has_played = true
+		timer.start()
+		print("timer start")
+		
+func _spawn_splash_exit(x: float):
+	var splash = _splash.instantiate()
+	add_child(splash)
+	splash.global_position.x = x
+	if _splash_exit_has_played == true:
+		return
+	else:
+		_splash_exit.play()
+		print("splash 2")
+		_splash_exit_has_played = true
+		timer_2.start()
+		print("timer 2 start")
 
 func _on_body_entered(body: Node2D) -> void:
+	if body is TileMap:
+		return
+	_spawn_splash_enter(body.position.x)
 	if body is Character:
 		body.enter_water(position.y)
 
@@ -11,6 +48,17 @@ func _on_body_exited(body: Node2D) -> void:
 	if body is Character:
 		if body.position.y -float(Global.ppt) / 2 <= position.y:
 			body.exit_water()
+			_spawn_splash_exit(body.position.x)
 		else:
 			body.dive()
 			
+
+
+func _on_timer_timeout() -> void:
+	_splashsfx_has_played = false
+	print(" timeout") # Replace with function body.
+
+
+func _on_timer_2_timeout() -> void:
+	_splash_exit_has_played = false
+	 # Replace with function body.
